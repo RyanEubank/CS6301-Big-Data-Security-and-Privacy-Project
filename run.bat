0cba1ebdacd35ceebc376864514ddbe2a7a6f580
REubank <reubank79@gmail.com>
2025-07-19 18:44:49 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,15 @@
+@echo off
+
 javac util/src/*.java -d util/build
 javac -classpath util/build inference/src/*.java -d inference/build
 javac -classpath util/build statistics/src/*.java -d statistics/build
 
-IF "%~1" == "--statistics" (
+set module="%~1"
+
+IF %module% == "--statistics" (
     java -cp "statistics/build;util/build" statistics.src.Main %2 %3
-) ELSE IF "%~1" == "--inference" (
-    java -cp "inference/build;util/build" inference.src.Main %2 %3
+) ELSE IF %module% == "--inference" (
+    java -cp "inference/build;util/build" inference.src.Main %2 %3 %4 %5
+) ELSE (
+    echo "Unknown command."
 )
